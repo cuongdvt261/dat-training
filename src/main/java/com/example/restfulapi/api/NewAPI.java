122dc715640f006de82306e72388e476eae69bfe
Mai Dat <maidat@MacBook-Pro-cua-dat.local>
2023-05-29 23:20:33 +0700

    add test restful api

---
+++ src/main/java/com/example/restfulapi/api/NewAPI.java
@@ -0,0 +1,17 @@
+package com.example.restfulapi.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.restfulapi.dto.NewDTO;
+
+@Controller
+public class NewAPI {
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    @ResponseBody
+    public NewDTO createNew(@RequestBody NewDTO model) {
+        return model;
+    }
+}
