0953da3d8c6fd746bb3d0ba303d0fd829f532570
Duong Vu Thai Cuong <cuongdvt261@gmail.com>
2023-07-19 22:57:07 +0700

    feature/test temp

--- src/main/java/com/example/restfulapi/api/APIUser.java
+++ src/main/java/com/example/restfulapi/api/APIUser.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
